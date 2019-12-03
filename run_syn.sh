84b06f4c9e4ca1125f9196635ad2f2666e42dedc
Pengfei Zhou <zhoupengfei@itp.ac.cn>
2019-12-03 08:36:07 +0800

    run

--- run_syn.sh
+++ run_syn.sh
@@ -9,7 +9,7 @@ for ((i=1;i<=10;i+=1))
    vars=$(echo "scale=4; 0+$j / $var1" | bc)
    #./sbm infer  -n1000 -b 1000 -q2 -P0.3,3.0  -a $vars,3.0   -t 800   -i2 -R0.85
    python main_gcn.py  --epoch 200 --ne 10000 --nu 10000 --K 5 --c1 10  --c2 10 --eps1 0.1 --eps2 $vars --Q 5 --hidden 64 --wd 5e-4 --rho 0.05 --seed 0 --seed_model $var2 --alpha 0.2 --net sgcn --early_stop 25 --nheads 4 --dropout 0.5 --lr 0.005 --fname sgcn_c1=10_c2=10__eps2.txt --cuda 5
-
+   python bpgcn_jsbm.py  --epoch 200 --ne 10000 --nu 10000  --c1 10 --c2 10 --beta 2 --netdepth 10 --eps1 0.1 --eps2 $vars --Q 5  --rho 0.05  --seed_model $var2  --early_stop 20 --x 0.5 --y 0.5  --fname bpgcn_c1=10_c2=10__eps2.txt --cuda 3
  done
    
 
